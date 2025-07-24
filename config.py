aeaaa12c4f6fad5ec9c7cd5b3b422f53c35e8353
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-24 12:33:20 +0400

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", "7800338935"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "QrupBotlariDestek")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "QrupBotlari")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Yusifovadide")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "PersionalDestek")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "PersionalSupport")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "PersionalTeamBot")
 
 # Random Start Images
 IMG = [
